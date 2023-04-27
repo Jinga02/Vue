39d4cc0e348814d5d4e8adf20db387bfebe5b3ff
진재환 <jjh099@icloud.com>
2023-04-27 15:14:41 +0900

    Vue Intro

---
+++ 01_day_Vue_Intro/SSAFY/01_vue_intro_page25.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+  <!-- HTML -->
+  <div>
+    <p>Hello :)</p>
+  </div>
+</template>
+
+<script>
+  // JavaScript
+</script>
+
+<style>
+  /* CSS */
+  p {
+    color: black;
+  }
+</style>
