5544e73821dbb8a5531cb0733098de4e3ed35d65
진재환 <woghks7041@gamil.com>
2023-05-02 11:07:17 +0900

    Vue component

---
+++ 03_day_Vue_CLI/vue-cli/src/components/MyChild.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>This is child component</h3>
+    <h3>아래는 네이버로 가는 링크 입니다.</h3>
+    <a href="http://naver.com">NAVER</a>
+  </div>
+</template>
+
+<script>
+export default {
+    namd : 'MyChild'
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ 03_day_Vue_CLI/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="border">
+    <h1>This is MyComponent</h1>
+    <!-- 자식 컴포넌트 사용 -->
+    <MyChild />
+  </div>
+</template>
+
+<script>
+// 자식 컴포넌트 불러오기
+import MyChild from '@/components/MyChild'
+
+export default {
+    name : 'MyComponent',
+    // 자식 컴포넌트 등록
+    components : {
+      MyChild,
+    }
+}
+</script>
+
+<style>
+  .border {
+    border: solid;
+  }
+</style>
+
